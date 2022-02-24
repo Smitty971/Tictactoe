18fee4723dd3a21d4639492937125ea6e5643c5b
Smitty971 <jlsmitty2273@yahoo.com>
2022-02-23 22:53:27 -0500

    add initial message

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
 let currentPlayer = "x";
 let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => 'Player ${currentPlayer} has won!';
+const drawMessage = () => 'Game ended in a draw!';
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+
+}
+function handlePlayerChange() {
+
+}
+function handleResultValidation() {
+
+}
+function handleCellClick() {
+
+}
+function handleRestartGame() {
+    
+}
